0a771280dc52a9c88005a1193603eec8242d77e4
juan1417 <juan.machado210@pascualbravo.edu.co>
2026-03-11 05:59:01 -0500

    Eliminar archivos de autenticación y modelos no utilizados

--- back/auth/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'created_at', 'updated_at')
-        read_only_fields = ('created_at', 'updated_at')
